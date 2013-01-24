5bbda4830e85a232be6cd0580bd7f51403764eb8
Sam-Mauris Yong <mauris@hotmail.sg>
2013-01-24 22:25:28 +0800

    IoC consumer abstraction

---
+++ src/Packfire/FuelBlade/IConsumer.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Packfire FuelBlade
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) Sam-Mauris Yong <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\FuelBlade;
+
+/**
+ * Interfacing of a service consumer
+ * 
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright Sam-Mauris Yong <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ * @package Packfire\FuelBlade
+ * @since 1.0.0
+ */
+
+interface IConsumer {
+    
+    public function __invoke($container);
+    
+}
