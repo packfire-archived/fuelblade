8ebdfa03de5dc66570ba76523b8a814a0dfac6ec
Sam-Mauris Yong <mauris@hotmail.sg>
2013-02-28 10:53:04 +0800

    in code doc updates for IContainer

--- src/Packfire/FuelBlade/IContainer.php
+++ src/Packfire/FuelBlade/IContainer.php
@@ -22,17 +22,46 @@ use \Closure;
  * @package Packfire\FuelBlade
  * @since 1.0.0
  */
-
 interface IContainer {
     
+    /**
+     * Create a function for creating an instance of a class
+     * @param string $class The class name to create
+     * @return Closure Returns the anonymous function that clones the object
+     * @since 1.0.0
+     */
     public function instance($class);
     
+    /**
+     * Create a function that stores a shared value
+     * @param Closure $callable The closure that creates the value to be shared
+     * @return Closure Returns the anonymous function that encapsulates the creation process
+     * @since 1.0.0
+     */
     public function share(Closure $callable);
     
+    /**
+     * Create a function for storing a Closure
+     * @param Closure The closure to be stored.
+     * @return Closure Returns the anonymous function that stores the original Closure
+     * @since 1.0.0
+     */
     public function func(Closure $callable);
     
+    /**
+     * Create a function for cloning an object
+     * @param object $object The object to be cloned when the function is called
+     * @return Closure Returns the anonymous function that clones the object
+     * @since 1.0.0
+     */
     public function copy($object);
     
+    /**
+     * Get a value from the container
+     * @param string $id The key of the value
+     * @return mixed Returns the value
+     * @since 1.0.0
+     */
     public function value($key);
     
 }
