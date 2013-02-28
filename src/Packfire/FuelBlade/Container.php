8215a72becafd0596b97477cfb2d87f98c4d9734
Sam-Mauris Yong <mauris@hotmail.sg>
2013-02-28 10:49:52 +0800

    in-code documentation updates in Container

--- src/Packfire/FuelBlade/Container.php
+++ src/Packfire/FuelBlade/Container.php
@@ -25,12 +25,31 @@ use \Closure;
 
 class Container implements IContainer, \ArrayAccess {
     
+    /**
+     * The values stored in the IoC container
+     * @var array
+     * @since 1.0.0
+     */
     protected $values = array();
     
+    /**
+     * Provides implementation of offsetExists in \ArrayAccess
+     * @internal
+     * @param string $id
+     * @return boolean
+     * @since 1.0.0
+     */
     public function offsetExists($id) {
         return isset($this->values[$id]);
     }
 
+    /**
+     * Provides implementation of offsetGet in \ArrayAccess
+     * @internal
+     * @param string $id
+     * @return mixed
+     * @since 1.0.0
+     */
     public function offsetGet($id) {
         if (!isset($this->values[$id])) {
             throw new \InvalidArgumentException(sprintf('"%s" is not defined in FuelBlade IoC container.', $id));
@@ -40,6 +59,12 @@ class Container implements IContainer, \ArrayAccess {
         return $this->loadValue($value);
     }
     
+    /**
+     * Load a value to see if it can be invoked to get the actual value
+     * @param mixed $value The value to be loaded
+     * @return mixed Returns the value loaded
+     * @since 1.0.0
+     */
     protected function loadValue($value){
         if(is_object($value) && method_exists($value, '__invoke')){
             return $value($this);
@@ -48,32 +73,69 @@ class Container implements IContainer, \ArrayAccess {
         }
     }
 
+    /**
+     * Provides implementation of offsetSet in \ArrayAccess
+     * @internal
+     * @param string $id
+     * @param mixed $value
+     * @since 1.0.0
+     */
     public function offsetSet($id, $value) {
         $this->values[$id] = $value;
     }
 
+    /**
+     * Provides implementation of offsetUnset in \ArrayAccess
+     * @internal
+     * @param string $id
+     * @since 1.0.0
+     */
     public function offsetUnset($id) {
         unset($this->values[$id]);
     }
-    
+
+    /**
+     * Create a function for cloning an object
+     * @param object $object The object to be cloned when the function is called
+     * @return Closure Returns the anonymous function that clones the object
+     * @since 1.0.0
+     */
     public function copy($object) {
         return function () use ($object) {
             return clone $object;
         };
     }
-
+    
+    /**
+     * Create a function for storing a Closure
+     * @param Closure The closure to be stored.
+     * @return Closure Returns the anonymous function that stores the original Closure
+     * @since 1.0.0
+     */
     public function func(Closure $callable) {
         return function()use(&$callable){
             return $callable;
         };
     }
-
+    
+    /**
+     * Create a function for creating an instance of a class
+     * @param string $class The class name to create
+     * @return Closure Returns the anonymous function that clones the object
+     * @since 1.0.0
+     */
     public function instance($class) {
         return function()use($class){
             return new $class();
         };
     }
-
+    
+    /**
+     * Create a function that stores a shared value
+     * @param Closure $callable The closure that creates the value to be shared
+     * @return Closure Returns the anonymous function that encapsulates the creation process
+     * @since 1.0.0
+     */
     public function share(Closure $callable) {
         return function ($c) use ($callable) {
             static $object = null;
@@ -86,6 +148,12 @@ class Container implements IContainer, \ArrayAccess {
         };
     }
     
+    /**
+     * Get a value from the container
+     * @param string $id The key of the value
+     * @return mixed Returns the value
+     * @since 1.0.0
+     */
     public function value($id){
         return $this->values[$id];
     }
