01223251051b796bcc7f3f1d2dbeb6bbb7183411
Sam-Mauris Yong <mauris@hotmail.sg>
2013-06-13 15:27:11 +0800

    updated tests and completed code coverage for serviceloader

---
+++ test/Packfire/FuelBlade/ConsumerFixture.php
@@ -0,0 +1,17 @@
+<?php
+namespace Packfire\FuelBlade;
+
+class ConsumerFixture implements ConsumerInterface
+{
+    protected $container;
+
+    public function container()
+    {
+        return $this->container;
+    }
+
+    public function __invoke($container)
+    {
+        $this->container = $container;
+    }
+}
--- test/Packfire/FuelBlade/ServiceLoaderTest.php
+++ test/Packfire/FuelBlade/ServiceLoaderTest.php
@@ -48,4 +48,52 @@ class ServiceLoadertest extends \PHPUnit_Framework_TestCase
         ServiceLoader::load($container, $services);
         $this->assertTrue(isset($container['test']));
     }
+
+    public function testLoadConsumer()
+    {
+        $services = array(
+            'test' => array(
+                'class' => '\\Packfire\\FuelBlade\\ConsumerFixture'
+            )
+        );
+        $container = new Container();
+        ServiceLoader::load($container, $services);
+        $this->assertTrue(isset($container['test']));
+        $this->assertInstanceOf('\\Packfire\\FuelBlade\\ConsumerFixture', $container['test']);
+        $this->assertEquals($container, $container['test']->container());
+    }
+
+    public function testLoadNonExists()
+    {
+        $services = array(
+            'test' => array(
+                'class' => '\\Packfire\\Dummy\\Mistake\\You\\No\\Me'
+            )
+        );
+        $container = new Container();
+        ServiceLoader::load($container, $services);
+        $this->assertTrue(isset($container['test']));
+        $this->assertNull($container['test']);
+    }
+    public function testLoadServiceParameters()
+    {
+        $services = array(
+            'test' => array(
+                'class' => '\\ReflectionMethod',
+                'parameters' => array(
+                    '@main',
+                    'testLoadWithParameters'
+                )
+            ),
+            'main' => array(
+                'class' => '\\Packfire\\FuelBlade\\ServiceLoaderTest'
+            )
+        );
+        $container = new Container();
+        ServiceLoader::load($container, $services);
+        $this->assertTrue(isset($container['test']));
+        $this->assertInstanceOf('\\ReflectionMethod', $container['test']);
+        $this->assertEquals(array(), $container['test']->getParameters());
+        $this->assertEquals('testLoadWithParameters', $container['test']->getName());
+    }
 }
